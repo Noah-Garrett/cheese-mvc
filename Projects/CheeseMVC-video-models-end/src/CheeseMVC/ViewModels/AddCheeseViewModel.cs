dc6391375c092ca8c5b03611e9e89e18b32af154
Noah Garrett <garretns@gmail.com>
2020-01-07 13:46:28 -0600

    dank

---
+++ Projects/CheeseMVC-video-models-end/src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCheeseViewModel
+    {
+        [Required]
+        [Display(Name = "Cheese Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "DAM DOOD GOTTA LABEL EET")]
+        public string Description { get; set; }
+
+        public CheeseType Type { get; set; }
+
+        public List<SelectListItem> CheeseTypes { get; set; }
+
+        [Range(1,5)]
+        public int Rating { get; set; }
+
+        public AddCheeseViewModel()
+        {
+            CheeseTypes = new List<SelectListItem>
+            {
+
+                //<option value="0">hard</option>
+                new SelectListItem()
+                {
+                    Value = ((int)CheeseType.Fake).ToString(),
+                    Text = CheeseType.Fake.ToString()
+                },
+
+                new SelectListItem()
+                {
+                    Value = ((int)CheeseType.Soft).ToString(),
+                    Text = CheeseType.Soft.ToString()
+                },
+
+                new SelectListItem()
+                {
+                    Value = ((int)CheeseType.Hard).ToString(),
+                    Text = CheeseType.Hard.ToString()
+                }
+            };
+        }
+
+        public Cheese CreateCheese()
+        {
+            return new Cheese
+            {
+                
+                Name = this.Name,
+                Description = this.Description,
+                Type = this.Type,
+                Rating = this.Rating
+             };
+        
+        }
+
+    }
+
+}
---
+++ Projects/CheeseMVC-video-models-end/src/CheeseMVC/ViewModels/EditAddCheeseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CheeseMVC.Models;
+namespace CheeseMVC.ViewModels
+{
+    public class EditAddCheeseViewModel : AddCheeseViewModel
+    {
+        public int CheeseId { get; set; }
+
+        public EditAddCheeseViewModel()
+        {
+        }
+        public EditAddCheeseViewModel(Cheese ch)
+        {
+            CheeseId = ch.CheeseId;
+            Name = ch.Name;
+            Description = ch.Description;
+            Type = ch.Type;
+            Rating = ch.Rating;
+        }
+    }
+}
